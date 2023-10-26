6f9378e3e830d56d60f28ad0ef84ada99c184fea
Diogo010101 <90548278+Diogo010101@users.noreply.github.com>
2023-10-26 15:01:38 -0300

    menu responsivo criado e pequenas mudanças no layout de header com media query

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
 
+var objMenuHeader = document.querySelector("#menu_principal");
 
+
+
+function dropMenu(){
+    if(objMenuHeader.style.display == 'none'){
+        objMenuHeader.style.display = 'block'
+    }else{
+        objMenuHeader.style.display = 'none'
+    }
+}
+
+
+function ajusteMenu(){
+    if(window.innerWidth >= 793){
+        objMenuHeader.style.display = 'block'
+    }else{
+        objMenuHeader.style.display = 'none'
+    }
+}
+
+
+
+
+/*Função botão de topo*/
 function botaoSubir(){
     window.scroll(0,0);
 }
